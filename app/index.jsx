0ca2737db0a7bafb3497f90415d90250ff25ddbb
LucassantoSCruz <112666059+LucassantoSCruz@users.noreply.github.com>
2025-09-24 12:36:39 -0300

    Adição do SVG e Timer funcionando
    Adição da biblioteca SVG e Timer funcionando.
    Encerramento do primeiro curso de React Native com Expo.

--- app/index.jsx
+++ app/index.jsx
@@ -1,25 +1,26 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Image, StyleSheet, Text, View, Pressable } from "react-native";
 import { FokusButton } from "../components/FokusButton"
 import { ActionButton } from "../components/ActionButton"
 import { Timer } from '../components/Timer'
+import { IconPause, IconPlay } from '../components/Icons'
 
 const pomodoro = [
   {
     id: 'focus',
-    initialValue: 25,
+    initialValue: 25 * 60,
     image: require('./pomodoro.png'),
     display: 'Foco'
   },
   {
     id: 'short',
-    initialValue: 5,
+    initialValue: 5 * 60,
     image: require('./short.png'),
     display: 'Pausa curta'
   },
   {
     id: 'long',
-    initialValue: 15,
+    initialValue: 15 * 60,
     image: require('./long.png'),
     display: 'Pausa longa'
   }
@@ -28,23 +29,65 @@ const pomodoro = [
 export default function Index() {
 
   const [timerType, setTimerType] = useState(pomodoro[0])
+  const [seconds, setSeconds] = useState(pomodoro[0].initialValue)
+  const [timerRunning, setTimerRunning] = useState(false)
+
+  const timerRef = useRef(null)
+
+  const clear = () => {
+    if (timerRef.current != null) {
+      clearInterval(timerRef.current)
+      timerRef.current = null
+      setTimerRunning(false)
+    }
+  }
+
+  const toogleTimerType = (newTimerType) => {
+    setTimerType(newTimerType)
+    setSeconds(newTimerType.initialValue)
+    clear()
+  }
+
+  const toogleTimer = () => {
+    if (timerRef.current) {
+      clear()
+      return
+    }
+
+    setTimerRunning(true)
+
+    const id = setInterval(() => {
+      setSeconds(oldState => {
+        if (oldState === 0) {
+          clear()
+          return timerType.initialValue
+        }
+        return oldState - 1
+      })
+    }, 1000)
+    timerRef.current = id
+  }
 
   return (
     <View style={styles.container}>
-      <Image source={timerType.image} style={{ width: '30%', height: '30%' }} />
+      <Image source={timerType.image} style={{ width: '100%' }} />
       <View style={styles.actions}>
         <View style={styles.context}>
           {pomodoro.map(p => (
             <ActionButton
               key={p.id}
               active={timerType.id === p.id}
-              onPress={() => setTimerType(p)}
+              onPress={() => toogleTimerType(p)}
               display={p.display}
             />
           ))}
         </View>
-        <Timer totalSeconds={timerType.initialValue}/>
-        <FokusButton />
+        <Timer totalSeconds={seconds} />
+        <FokusButton
+          title={timerRunning ? 'Pausar' : 'Começar'}
+          icon={timerRunning ? <IconPause/> : <IconPlay/>}
+          onPress={toogleTimer}
+        />
       </View>
       <View style={styles.footer}>
         <Text style={styles.footerText}>
