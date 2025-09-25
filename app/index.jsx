c2864a15aac1d36859a021fe9e1d0edc86c6de85
LucassantoSCruz <112666059+LucassantoSCruz@users.noreply.github.com>
2025-09-25 13:49:11 -0300

    Criação da tela inicial e organização de imagens

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,63 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Text, Image, View, StyleSheet } from "react-native";
+import { FokusButton } from "../components/FokusButton"
 
 export default function Index() {
   return (
-    <View>
-      <Text>
-        Home
-      </Text>
-      <Link href={{ pathname: '/pomodoro' }}>
-        Quero Iniciar!
-      </Link>
+    <View style={styles.container}>
+      <Image source={require('../assets/images/logo.png')} />
+      <View style={styles.inner}>
+        <Text style={styles.title}>
+          Otimize sua {'\n'} produtividade, {'\n'}
+          <Text style={styles.bold}>mergulhe no que {'\n'} importa</Text>
+        </Text>
+        <Image source={require('../assets/images/home.png')} style={styles.homeImage} />
+        <FokusButton
+          title={"Quero iniciar"}
+        />
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Projeto fictício e sem fins comerciais.
+        </Text>
+        <Text style={styles.footerText}>
+          Desenvolvido por Alura.
+        </Text>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#021123',
+    gap: 40,
+  },
+  inner: {
+    gap: 16
+  },
+  title: {
+    color: '#FFFFFF',
+    textAlign: 'center',
+    fontSize: 26
+  },
+  bold: {
+    fontWeight: 'bold'
+  },
+  footer: {
+    width: '80%'
+  },
+  footerText: {
+    textAlign: 'center',
+    color: '#98A0A8',
+    fontSize: 12.4
+  },
+  homeImage: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+  },
+})
