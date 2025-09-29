2ac005a2a7b79828c79d5ae73a3cea3c68f4e611
LucassantoSCruz <112666059+LucassantoSCruz@users.noreply.github.com>
2025-09-29 09:20:07 -0300

    Update add-task
    Criação e estilização de componentes na tela.

--- app/add-task/index.jsx
+++ app/add-task/index.jsx
@@ -1,25 +1,77 @@
-import { Pressable, Text, TextInput, View } from "react-native";
+import { KeyboardAvoidingView, Pressable, Text, TextInput, View, StyleSheet, Platform, TouchableWithoutFeedback, Keyboard } from "react-native";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { IconSave } from "../../components/Icons";
 
 export default function AddTask() {
     return (
-        <SafeAreaView>
-            <Text>
-                Adicionar uma tarefa
-            </Text>
-            <View>
-                <Text>
-                    Em que você está trabalhando?
-                </Text>
-                <TextInput />
-                <Pressable>
-                    <IconSave />
-                    <Text>
-                        Salvar
+        <KeyboardAvoidingView
+            style={styles.container}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        >
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss} >
+                <View style={styles.inner}>
+                    <Text style={styles.text}>
+                        Adicionar uma tarefa
                     </Text>
-                </Pressable>
-            </View>
-        </SafeAreaView>
+                    <Text style={styles.label}>
+                        Em que você está trabalhando?
+                    </Text>
+                    <TextInput
+                        style={styles.input}
+                        numberOfLines={10}
+                        multiline={true}
+                    />
+                    <View style={styles.actions}>
+                        <Pressable style={styles.button}>
+                            <IconSave />
+                            <Text>
+                                Salvar
+                            </Text>
+                        </Pressable>
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#021123',
+        gap: 16,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text: {
+        color: '#FFF',
+        textAlign: 'center',
+        fontSize: 26
+    },
+    inner: {
+        backgroundColor: '#98A0A8',
+        width: '90%',
+        borderRadius: 8,
+        padding: 16,
+        gap: 32
+    },
+    label: {
+        fontWeight: 600,
+        fontSize: 18
+    },
+    input: {
+        backgroundColor: '#FFF',
+        padding: 16,
+        borderRadius: 8,
+        height: 100
+    },
+    button: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 4
+    },
+    actions: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
+    }
+})
